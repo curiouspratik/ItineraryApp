268593ae79c0aec588f92b0be5e4d4aedc38a896
Pratik Patil <patil.pratiq@gmail.com>
2024-03-12 13:31:18 -0400

    Initial Commit

---
+++ ItineraryApp/ItineraryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItineraryAppApp.swift
+//  ItineraryApp
+//
+//  Created by Pratik Ashok Patil on 12/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ItineraryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
